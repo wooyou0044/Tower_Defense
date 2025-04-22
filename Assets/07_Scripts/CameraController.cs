b82b9e9dc3e0b5454b0e632d8a708baccbd03869
wooyou0044 <chawoo2206@gmail.com>
2025-04-22 19:09:55 +0900

    [Feat] 카메라 움직임, 미니맵 제작 완료

--- Assets/07_Scripts/CameraController.cs
+++ Assets/07_Scripts/CameraController.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float zoomSpeed;
+    [SerializeField] float minZoomZ;
+    [SerializeField] float maxZoomZ;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float edgeSize;
+    [SerializeField] float moveDistance;
+    [SerializeField] float rotateSpeed;
+
+    Camera cam;
+
+    float originZoomSpeed;
+    bool isMoving;
+    Vector3 targetPos;
+
+    private void Awake()
     {
-        
+        cam = GetComponent<Camera>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        originZoomSpeed = zoomSpeed;
+    }
+
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = Vector3.zero;
+
+        if(Input.mousePosition.x >= Screen.width - edgeSize)
+        {
+            moveDir += Vector3.right;
+        }
+        if(Input.mousePosition.x <= edgeSize)
+        {
+            moveDir += Vector3.left;
+        }
+        if(Input.mousePosition.y >= Screen.height - edgeSize)
+        {
+            moveDir += Vector3.up;
+        }
+        if(Input.mousePosition.y <= edgeSize)
+        {
+            moveDir += Vector3.back;
+        }
+
+        Vector3 move = (new Vector3(h, v, 0) + moveDir) * moveSpeed * Time.deltaTime;
+
+        transform.Translate(move, Space.World);
+
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if(scroll != 0)
+        {
+            cam.orthographicSize -= scroll * zoomSpeed;
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minZoomZ, maxZoomZ);
+        }
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.up, -rotateSpeed * Time.deltaTime, Space.World);
+        }
+
+        if(Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.World);
+        }
     }
+
 }
