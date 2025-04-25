b5b5d53df0ba02164a217c161b886be1cd03fc05
wooyou0044 <chawoo2206@gmail.com>
2025-04-25 19:51:56 +0900

    [Feat] 미니맵 격자로 나누기 완성

--- Assets/07_Scripts/MiniMapManager.cs
+++ Assets/07_Scripts/MiniMapManager.cs
@@ -21,10 +21,10 @@ public class MiniMapManager : MonoBehaviour
             }
         }
 
-        foreach(Transform child in transform)
+        foreach (Transform child in transform)
         {
             TileNode node = child.GetComponent<TileNode>();
-            if(node == null || node.type == TileType.Road)
+            if (node == null || node.type == TileType.Road)
             {
                 Debug.Log(node.gameObject);
                 continue;
@@ -32,28 +32,198 @@ public class MiniMapManager : MonoBehaviour
             Vector3 pos = child.localPosition;
             Vector3 scale = child.localScale;
 
+            bool isAbandon = false;
+
             Debug.Log("node.gridSize : " + node.gridSize);
             int width = Mathf.FloorToInt(scale.x / node.gridSize);
             int height = Mathf.FloorToInt(scale.z / node.gridSize);
 
-            int baseX = Mathf.RoundToInt(pos.x / 4);
-            int baseZ = Mathf.RoundToInt(-pos.z / 4);
+            int gridPosX = 0;
+            int gridPosZ = 0;
+            int baseX;
+            int baseZ;
+            //if ((pos.x / 4 * 10) % 10 > 5f)
+            //{
+            //    baseX = Mathf.RoundToInt(pos.x / 4);
+            //}
+            //else
+            //{
+            //    baseX = Mathf.FloorToInt(pos.x / 4);
+            //}
 
-            for(int i=0; i<height; i++)
+            //if ((pos.x / 4) < 1 && (pos.x % 4) > 0.5f)
+            //{
+            //    baseX = Mathf.FloorToInt(pos.x / 4);
+            //}
+            //else
+            //{
+            //    baseX = Mathf.RoundToInt(pos.x / 4);
+            //}
+            if (width == indexSize)
             {
-                for(int j=0; j<width; j++)
+                baseZ = Mathf.FloorToInt(-pos.z / 4);
+                Debug.Log(baseZ);
+                for (int i = 0; i < width; i++)
                 {
-                    int x = baseZ + i;
-                    int z = baseX + j;
+                    tileNodes[baseZ, i].type = node.type;
+                }
+                continue;
+            }
 
-                    if(x >= 0 && x <indexSize && z >= 0 && z <indexSize)
+            //if (width > height && (pos.x / 4 * 10) % 10 >= 5f)
+            //{
+            //    baseX = Mathf.FloorToInt(pos.x / 4);
+            //    isAbandon = true;
+            //}
+            //else if(width == height && (pos.x / 4 * 10) % 10 == 5f)
+            //{
+            //    baseX = Mathf.FloorToInt(pos.x / 4);
+            //}
+            //else
+            //{
+            //    baseX = Mathf.RoundToInt(pos.x / 4);
+            //}
+
+            //if(width > height && (-pos.z / 4 * 10) % 10 > 5f)
+            //{
+            //    baseZ = Mathf.RoundToInt(-pos.z / 4);
+            //}
+            //// 클지 작을지는 확인해봐야 함
+            //else if (width == height && (-pos.z / 4 * 10) % 10 > 5f)
+            //{
+            //    baseZ = Mathf.RoundToInt(-pos.z / 4);
+            //}
+            //else
+            //{
+            //    baseZ = Mathf.FloorToInt(-pos.z / 4);
+            //}
+
+            ////int baseX = Mathf.FloorToInt(pos.x / 4);
+            ////baseZ = Mathf.FloorToInt(-pos.z / 4);
+
+            //if(width >= 3 && isAbandon == false)
+            //{
+            //    baseX -= 1;
+            //}
+            //if (height >= 3)
+            //{
+            //    //baseZ -= 1;
+            //    baseZ -= Mathf.FloorToInt(height / node.gridSize);
+            //}
+
+            //baseX = Mathf.FloorToInt(pos.x) / 4;
+            //baseZ = Mathf.FloorToInt(-pos.z) / 4;
+
+            if(pos.x % 4 == 0)
+            {
+                baseX = Mathf.FloorToInt(pos.x / 4);
+            }
+
+            else
+            {
+                // 4X4 => 0     1
+                // 8X8 => 2     2
+                // 12x12 => 4   3
+                // 16X16 => 6   4
+                // 20X20 => 8   5
+                if(width % 2 == 0)
+                {
+                    // 이건 있어야 할지 빼야 할지 이따가 결정
+                    if(pos.x % 4 == 2)
                     {
-                        tileNodes[x, z].type = node.type;
+                        baseX = Mathf.FloorToInt(pos.x / 4);
+                    }
+                    // 4로 나눈 나머지가 2로 만들어야 함
+                    //if(pos.x % 4 >=1)
+                    //{
+                    //    baseX = Mathf.FloorToInt(pos.x + (2 - pos.x % 4));
+                    //}
+                    //else
+                    //{
+                    //    baseX = Mathf.FloorToInt(pos.x - (2 - pos.x % 4));
+                    //}
+                    else
+                    {
+                        gridPosX = Mathf.FloorToInt(pos.x + (2 - pos.x % 4));
+                        baseX = gridPosX / 4;
+                    }
+                }
+                else
+                {
+                    // 4로 나눈 나머지가 0으로 만들어야 함
+                    if(pos.x % 4 >= 2)
+                    {
+                        gridPosX = Mathf.FloorToInt(pos.x - (pos.x % 4)) + 4;
+                        baseX = gridPosX / 4;
+                    }
+                    else
+                    {
+                        gridPosX = Mathf.FloorToInt(pos.x - (pos.x % 4));
+                        baseX = gridPosX / 4;
                     }
                 }
             }
 
-            // 높이는 나중에
+            if (-pos.z % 4 == 0)
+            {
+                baseZ = Mathf.FloorToInt(-pos.z / 4);
+            }
+
+            else
+            {
+                if (height % 2 == 0)
+                {
+                    // 4로 나눈 나머지가 2로 떨어지게 해야 함
+                    if (-pos.z % 4 == 2)
+                    {
+                        baseZ = Mathf.FloorToInt(-pos.z / 4);
+                    }
+                    // 4로 나눈 나머지가 2로 만들어야 함
+                    //if(pos.x % 4 >=1)
+                    //{
+                    //    baseX = Mathf.FloorToInt(pos.x + (2 - pos.x % 4));
+                    //}
+                    //else
+                    //{
+                    //    baseX = Mathf.FloorToInt(pos.x - (2 - pos.x % 4));
+                    //}
+                    else
+                    {
+                        gridPosZ = Mathf.FloorToInt(-pos.z - (2 - (-pos.z % 4)));
+                        baseZ = gridPosZ / 4;
+                    }
+                }
+                else
+                {
+                    // 4로 나눈 나머지가 0을 떨어지게 해야 함
+                    if (-pos.z % 4 >= 2)
+                    {
+                        gridPosZ = Mathf.FloorToInt(-pos.z - (-pos.z % 4)) + 4;
+                        baseZ = gridPosZ / 4;
+                    }
+                    else
+                    {
+                        gridPosZ = Mathf.FloorToInt(-pos.z - (-pos.z % 4));
+                        baseZ = gridPosZ / 4;
+                    }
+                }
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    int z = baseZ + i;
+                    int x = baseX + j;
+                    if (x >= 0 && x < indexSize && z >= 0 && z < indexSize)
+                    {
+                        tileNodes[z, x].type = node.type;
+                    }
+                }
+            }
+
+            //    // 높이는 나중에
+
         }
 
         for(int i=0; i<indexSize; i++)
@@ -68,5 +238,17 @@ public class MiniMapManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void MakeTileSizeGrid(int size)
+    {
+        if(size % 2== 0)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
